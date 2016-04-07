bd1744ebe44624ab44f7a377fbaca9401ef7435a
Alobin Technologies <support@alobin.com>
2016-04-07 12:41:12 +0530

    updated the invite create and join

---
+++ resources/views/invite/join.blade.php
@@ -0,0 +1,16 @@
+@extends("layouts.app")
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="panel panel-default">
+                <div class="panel-heading">Accept Invitation</div>
+                <div class="panel-body">
+                  <h1>You have been invited to join {{ $invitation->account->name }}</h1>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
